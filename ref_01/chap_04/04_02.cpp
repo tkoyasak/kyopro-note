a970e3197dba1a166a3ea20cb1ff97655c0ef5e6
Takahiro Koyasako <tkoyasak@student.42tokyo.jp>
2021-04-11 09:22:58 +0900

    rename

---
+++ ref_01/chap_04/04_02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> memo;
+
+long long	tribo(int N)
+{
+	if (N == 0 || N == 1)
+		return 0;
+	else if (N == 2)
+		return 1;
+	if (memo[N] != -1)
+		return memo[N];
+	return memo[N] = tribo(N - 1) + tribo(N - 2) + tribo(N - 3);
+}
+
+int	main(void)
+{
+	int N;
+	cin >> N;
+	memo.assign(N + 1, -1);
+	cout << tribo(N) << endl;
+}
