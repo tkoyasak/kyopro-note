5a0e3f19c6f205d48db9dde498f12083b4ae722a
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-10-20 22:16:05 +0900

    [update]

--- tpl/contest/tpl.cpp
+++ tpl/contest/tpl.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 #define all(obj) (obj).begin(),(obj).end()
 #define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define reps(i,a,n) for(long long i=(a);i<(long long)(n);++i)
+#define rep(i,n) reps(i,0,n)
 
 template<class T> inline bool chmin(T& a, T b) {if(a>b){a=b;return(true);}return(false);}
 template<class T> inline bool chmax(T& a, T b) {if(a<b){a=b;return(true);}return(false);}
