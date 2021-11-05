a0c59c210acc86220399518d1f7c00efed9d159e
Takahiro Koyasako <wr.pfxx15qs10@gmail.com>
2021-11-05 14:37:05 +0900

    style: rename .tpl && delete .editorconfig

--- .tpl/contest/tpl.cpp
+++ .tpl/contest/tpl.cpp
@@ -8,6 +8,8 @@ using namespace std;
 #define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
 #define reps(i,a,n) for(long long i=(a);i<(long long)(n);++i)
 #define rep(i,n) reps(i,0,n)
+#define rrep(i,a,n) for(long long i=(a);i>=(long long)(n);--i)
+#define fore(i,a) for(auto &i:(a))
 
 template<class T> inline bool chmin(T& a, T b) {if(a>b){a=b;return(true);}return(false);}
 template<class T> inline bool chmax(T& a, T b) {if(a<b){a=b;return(true);}return(false);}
@@ -15,14 +17,15 @@ template<class T> inline bool chmax(T& a, T b) {if(a<b){a=b;return(true);}return
 using ll=long long;
 using ull=unsigned long long;
 using ld=long double;
-using vll=vector<ll>;
+using vi=vector<int>;
+using vll=vector<long long>;
 using vs=vector<string>;
-const int inf = 1e9 + 10;
+const int inf = 1e9+10;
 const ll INF = 1e18;
 const ld EPS = 1e-10;
 
 
 int	main(void)
 {
-	//
+  //
 }
